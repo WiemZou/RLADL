95f0271d4a62eb1b3598c0755de1a7e4c81b2edb
WiemZou <wiem.zouali@gmail.com>
2019-11-04 19:54:11 +0100

    Add files via upload

---
+++ TME2-3/TME2.py
@@ -0,0 +1,166 @@
+import matplotlib
+
+matplotlib.use("TkAgg")
+import gym
+import gridworld
+from gym import wrappers, logger
+import numpy as np
+
+
+import random
+
+#gamma=1 toutes politiques ont le meme coup meme interet quel que soit le chemin choisi alors que pour gamma inferieur a 1 on force a choisir le plus court
+
+#env.render()
+
+def EvalPolicy(states,P,V,gamma): #a faire
+    states_P=list(P.keys())
+    pi=dict()
+        
+    for s in states_P:
+        act = np.zeros(len(P[s])) # erreur avec etats terminaux a voir  
+        for a in P[s]:
+            for dest in range(len(P[s][a])):
+                p = P[s][a][dest][0]
+                r = P[s][a][dest][2]
+                s_dest = P[s][a][dest][1]
+                if s_dest in states_P:
+                    d=states_P.index(s_dest)
+                    act[a] += p*(r + gamma*V[d])
+        
+        pi[states[s]] = np.argmax(act)
+
+    return pi        
+        
+
+def compare_dict(dict1,dict2): #fonction a tester 
+    for s in dict1:
+        if dict1[s] != dict2[s]:
+            return False
+    return True       
+    
+
+
+def PolicyIteration(env,eps,gamma):
+    states, P = env.getMDP()
+    #pi et V de dimension P nombre d'etats non terminaux car terminaux par defaut c'est 0 on ne peut pas en sortir
+    states_P=list(P.keys())
+    pi0=dict()
+    for s in states_P:
+        actions=list(P[s].keys())
+        act=random.randint(0,len(actions)-1)
+        pi0[states[s]]=actions[act]
+    
+    while 1:  
+        i=0
+        V0=np.array([random.random() for i in range(len(states_P))])
+        while 1:
+            V1=np.zeros(len(states_P))
+            for si in range(len(states_P)):
+                s=states_P[si]
+                act=pi0[states[s]]
+                for dest in P[s][act]:
+                    p,d,r,final=dest
+                    if d not in states_P:
+                        V1[si] += p*r
+                    else:
+                        d=states_P.index(d)
+                        
+                        V1[si] += p*(r+gamma*V0[d])
+#            print(V1)
+            i = i+1
+            if np.linalg.norm(V1-V0,ord=np.inf)<= eps:
+                V0 = np.copy(V1) 
+                break
+            V0 = np.copy(V1) 
+            
+        print('break')
+        pi1 = EvalPolicy(states,P,V0,gamma)
+        
+        if compare_dict(pi0,pi1):
+            print('end')
+            break
+        pi0=dict(pi1)
+        print('end2')
+        
+    return pi1
+
+
+def ValueIteration(env,eps,gamma):
+    states, P = env.getMDP()
+    states_P=list(P.keys())
+    V0=np.array([random.random() for i in range(len(states_P))])
+    i = 0 
+    while 1:
+        V1=np.zeros(len(states_P))
+        for si in range(len(states_P)):
+            s=states_P[si]
+            act = np.zeros(len(P[s]))
+            for a in P[s]:
+                for dest in P[s][a]:
+                    p,d,r,final=dest
+                    if d not in states_P:
+                        act[a]+=p*r
+                    else:
+                        d=states_P.index(d)
+                        act[a] += p*(r + gamma*V0[d])
+            V1[si] =  np.max(act)
+        i = i + 1 
+        print(np.linalg.norm(V1-V0,ord=np.inf))
+        if np.linalg.norm(V1-V0,ord=np.inf)<= eps:
+            V0 = np.copy(V1)
+            break
+        V0 = np.copy(V1) 
+        
+    pi = EvalPolicy(states,P,V0,gamma)
+    print('end')
+
+    return pi      
+
+class Agent():
+    def __init__(self, env,eps,gamma,algo):
+        self.env=env
+        self.eps=eps
+        self.gamma=gamma
+        self.pi=algo(self.env,self.eps,self.gamma)
+
+    def act(self, etat):
+        return self.pi[etat]
+        
+if __name__ == '__main__':
+
+    env = gym.make("gridworld-v0")
+    env.seed(0)  # Initialise le seed du pseudo-random
+    env.render()  # permet de visualiser la grille du jeu 
+    
+    etat_init=env.reset()
+    obs=env.state2str(etat_init)
+    states, P = env.getMDP()
+    # Faire un fichier de log sur plusieurs scenarios
+    outdir = 'gridworld-v0/random-agent-results'
+#    envm = wrappers.Monitor(env, directory=outdir, force=True, video_callable=False)
+    env.setPlan("gridworldPlans/plan0.txt", {0: -0.001, 3: 1, 4: 1, 5: -1, 6: -1})
+    eps = 0.01
+    rsum=0
+    j=0
+    i=0
+    gamma=0.75
+    agent = Agent(env,eps,gamma,ValueIteration)
+    while True:
+        action = agent.act(states[obs])
+        obs, reward, done, _ = env.step(action)
+        obs=env.state2str(obs)
+        rsum += reward
+        j += 1
+        env.render()
+        if done:
+            print("Episode : " + str(i) + " rsum=" + str(rsum) + ", " + str(j) + " actions")
+            break
+
+
+#plot des rewards en fonction des epoques, nb d'actions au fil du temps
+        
+ #politique dictionnaire ou chaque etat dis ou on doit aller
+ 
+# V value : dictionnaire
+# Fonction qui fait policy iteration p        act = np.zeros(len(P[s])) # erreur avec etats terminaux a voir  rend MDP
---
+++ TME2-3/TME3.py
@@ -0,0 +1,98 @@
+import matplotlib
+
+matplotlib.use("TkAgg")
+import gym
+import gridworld
+from gym import wrappers, logger
+import numpy as np
+import random
+
+
+
+#gamma=1 toutes politiques ont le meme coup meme interet quel que soit le chemin choisi alors que pour gamma inferieur à 1 on force a choisir le plus court
+
+#env.render()
+class Q_learning():
+    def __init__(self,gamma,alpha,eps,states,actions): #eps pour epsilon greedy
+        self.gamma=gamma
+        self.alpha=alpha
+        self.eps=eps
+        self.states=states
+        self.actions=actions
+        self.Q={s :{a : 0 for a in self.actions} for s in self.states}
+        
+    def act(self,etat):
+        v=random.random()
+        if v>eps:
+            qmax=dict()
+            for a in self.actions:
+                qmax[a]=self.Q[etat][a]
+            qmax = [(value, key) for key, value in qmax.items()]
+            amax = max(qmax)[1]
+            return amax
+        else:
+            return self.actions[np.random.randint(0,len(self.actions)-1)]
+    
+    def update(self,etat,act,obs,reward):
+        
+        qmax = []
+        for aprime in self.actions:
+            qmax.append(self.Q[obs][aprime]) 
+        qmax = max(qmax)
+        self.Q[etat][act] += self.alpha*(reward + self.gamma*qmax- self.Q[etat][act])
+        return
+
+
+        
+if __name__ == '__main__':
+
+    env = gym.make("gridworld-v0")
+    env.setPlan("gridworldPlans/plan0.txt", {0: -0.001, 3: 1, 4: 1, 5: -1, 6: -1})
+
+    env.seed(0)  # Initialise le seed du pseudo-random
+    env.render()  # permet de visualiser la grille du jeu   
+    etat_init=env.reset()
+    states, _ = env.getMDP()
+#    states=env.states
+    actions=list(env.actions.keys())  
+    
+
+    # Faire un fichier de log sur plusieurs scenarios
+    outdir = 'gridworld-v0/random-agent-results'
+#    envm = wrappers.Monitor(env, directory=outdir, force=True, video_callable=False)
+    env.seed()  # Initialiser le pseudo aleatoire
+    eps = 0.2
+    gamma=0.75
+    alpha=0.5
+    rsum=0
+    agent = Q_learning(gamma,alpha,eps,states,actions)
+    i=0
+    episode_count = 10000
+    for i in range(episode_count):
+        obs = env.reset()
+        obs=env.state2str(obs)
+        env.verbose = (i % 100 == 0 and i > 0)  # afficher 1 episode sur 100
+        if env.verbose:
+            env.render()
+        j = 0
+        rsum = 0
+        while True:
+            action = agent.act(obs)
+            newobs, reward, done, _ = env.step(action)
+            newobs=env.state2str(newobs)
+            agent.update(obs,action,newobs,reward)
+            rsum += reward
+            j += 1
+            env.render()
+            obs = newobs
+            if done:
+                print("Episode : " + str(i) + " rsum=" + str(rsum) + ", " + str(j) + " actions")
+                break
+
+
+    
+#Reward négatif :  etat rouge pénalisé 
+ #politique dictionnaire ou chaque etat dis ou on doit aller
+ 
+# V value : dictionnaire
+# Fonction qui fait policy iteration prend MDP
